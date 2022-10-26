18dc3f5188847e1ce08edc6a9604f7b75beb1306
LlGomez <lluisgomezf@gmail.com>
2022-10-26 19:11:51 +0200

    cambios

--- ProyectoJuegoMemoria/js/Juegomemoria.js
+++ ProyectoJuegoMemoria/js/Juegomemoria.js
@@ -23,73 +23,53 @@ function AppFuncionando() {
             }
         }
     
-        return arrayTablero
+        return arrayTablero;
     }
-    /*
+    
+    
     function colocarEmojis (arrayTablero,maxFilas, maxColumnas) {
-        let contadorEmojis = 0;
-        let posFila;
-        let posColumna;
+        let parejas = [
+            "imagenes/caraSonriente.png",
+            "imagenes/emoji2.png",
+            "imagenes/emoji3.png",
+            "imagenes/emoji4.png",
+            "imagenes/emoji5.png",
+            "imagenes/emoji6.png",
+            "imagenes/emoji7.png",
+            "imagenes/emoji8.png",
+            "imagenes/emoji9.png"];
+    let numCasillas = maxFilas * maxColumnas;
+    let posFila = 0;
+    let posColumna = 0;
+    let numParejas = 0;
+    let contadorArray = 0;
     
-    
-        while (contadorEmojis < numMinas) {
-            posFila = Math.floor(Math.random() * maxFilas);
-            posColumna = Math.floor(Math.random() * maxColumnas);
-    
-            if (arrayTablero[posFila][posColumna] != document.write('&#128512')) {
-                arrayTablero[posFila][posColumna] = document.write('&#128512');
-                contadorEmojis++;
-            };
-        };
-    
-        return arrayTablero
-    }
-    */
+        
 
-    /*
-    function contarEmojis (arrayTablero, maxFilas, maxColumnas) {
-        let numMinasAlrededor;
-    
-        for (let fila = 0; fila < maxFilas; fila++) {
-            for (let columna = 0; columna < maxColumnas; columna++) {
-                numMinasAlrededor = 0;
-                if (arrayTablero[fila][columna] != 'MINA') {
-                    for (let cFila = fila - 1; cFila <= fila + 1; cFila++) {
-                        if (cFila >= 0 && cFila < maxFilas) {
-                            for (let cColumna = columna - 1; cColumna <= columna + 1; cColumna++) {
-                                if (cColumna >= 0 && cColumna < maxColumnas &&
-                                    arrayTablero[cFila][cColumna] == 'MINA') {
-                                    numMinasAlrededor++;
-                                }
-                            }
-                        }
-                        arrayTablero[fila][columna] = numMinasAlrededor;
-                    }
-                }
-            }
-        }
-        return arrayTablero
-    }
-    */
+    return arrayTablero;
+
+}
+
    
-    function pintarTablero(tablero, filas, columnas) {
-    // Creamos el tablero en html
+    function pintarTablero(arrayTablero, maxFilas, maxColumnas){
+    
         document.write('<table>');
     
-        for (let i = 0; i < filas; i++) {
+        for (let i = 0; i < maxFilas; i++) {
+        
             document.write('<tr>');
-        
-            for (let j = 0; j < columnas; j++) {
-                document.write(`<td>${tablero[i][j]}</td>`);
+    
+            for (let j = 0; j < maxColumnas; j++) {
+            
+                document.write('<td><img src=' + arrayTablero[i][j] + '></td>');
+            
             }
-        
             document.write('</tr>');
         }
+    
         document.write('</table>');
     
     }
-    
 
 
-
-    AppFuncionando();
+AppFuncionando();
